bba91e2d1caa521646fed909b6d24825e430624b
Ehshan Veerabangsa <eveerabangsa@gmail.com>
2015-04-11 18:20:24 +0100

    added to Crawler interface - method to create absolute paths for relative links

--- src/Interfaces/Crawler.java
+++ src/Interfaces/Crawler.java
@@ -1,6 +1,7 @@
 package Interfaces;
 
 import java.net.URL;
+import java.net.MalformedURLException;
 
 /**
  * A class to scrape web-pages for links
@@ -14,7 +15,25 @@ public interface Crawler {
      * @param url the web page to be scraped for links
      * @param file the name of the file for stored links
      */
-    void crawl(URL url, String file);
+    void crawl(URL url, String file) throws MalformedURLException;
+
+    /**
+     * Establishes the homepath of the current webpage
+     *
+     * @param path the path of the link being analysed
+     * @return the the home path of the initial page
+     */
+    String homePath (String path);
+
+    /**
+     * Creates an absoulte path for link if its path is relative to home
+     *
+     * @param url the current url being visited
+     * @param link the path of the link being analysed
+     * @param homePath the homePath of the site
+     * @return the absolute path of the analysed link
+     */
+    String absoulutePath(String url, String link, String homePath);
 
     /**
      * Default search method
