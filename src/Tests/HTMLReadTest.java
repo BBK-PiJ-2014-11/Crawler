2ff829978f0bc027ca6ce834bdcc0a85c9fcf57f
Ehshan Veerabangsa <eveerabangsa@gmail.com>
2015-04-11 19:39:41 +0100

    added test setup

--- src/Tests/HTMLReadTest.java
+++ src/Tests/HTMLReadTest.java
@@ -4,18 +4,30 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
 import static org.junit.Assert.*;
 
 public class HTMLReadTest {
 
+    private HTMLRead reader;
+    private InputStream is;
+    private final File file = new File("htmlTest.html");
+
     @Before
-    public void setUp(){
+    public void setUp() throws FileNotFoundException {
+        reader = new HTMLRead();
+        is = new FileInputStream(file);
 
     }
 
     @After
     public void tearDown(){
-
+        reader = null;
+        is = null;
     }
 
     @Test
