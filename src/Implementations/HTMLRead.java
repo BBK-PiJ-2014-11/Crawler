42b0a4d3623606db9ffdc662e5ab55c3364ff23d
Ehshan Veerabangsa <eveerabangsa@gmail.com>
2015-04-14 18:42:38 +0100

    added ReadUntil method - all relevant test passed

--- src/Implementations/HTMLRead.java
+++ src/Implementations/HTMLRead.java
@@ -2,11 +2,25 @@ package Implementations;
 
 import Interfaces.Reader;
 
+import java.io.IOException;
 import java.io.InputStream;
 
 public class HTMLRead implements Reader {
+
     @Override
     public boolean readUntil(InputStream is, char ch1, char ch2) {
+        int x;
+        try {
+            while ((x = is.read()) != -1) {
+                if (Character.toLowerCase((char) x) == Character.toLowerCase(ch1)){
+                    return true;
+                }else if(Character.toLowerCase((char) x) == Character.toLowerCase(ch2)) {
+                   return false;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return false;
     }
 
