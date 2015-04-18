be14916cc253a6ba9fb586d406e2b50eedbf3e9c
Ehshan Veerabangsa <eveerabangsa@gmail.com>
2015-04-18 01:28:09 +0100

    added initial build of WebCrawlerTest

---
+++ src/Tests/WebCrawlerTest.java
@@ -0,0 +1,56 @@
+package Tests;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class WebCrawlerTest {
+
+    @Before
+    public void setUp() {
+
+    }
+
+    @After
+    public void tearDown()  {
+
+    }
+
+    @Test
+    public void testCrawl()  {
+
+    }
+
+    @Test
+    public void testGetLinks()  {
+
+    }
+
+    @Test
+    public void testParseURL()  {
+
+    }
+
+    @Test
+    public void testGetHomePath()  {
+
+    }
+
+    @Test
+    public void testSetAbsolutePath()  {
+
+    }
+
+    @Test
+    public void testCheckString()  {
+
+    }
+
+    @Test
+    public void testSearch()  {
+
+    }
+
+}
