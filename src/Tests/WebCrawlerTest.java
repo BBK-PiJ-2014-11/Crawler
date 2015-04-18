c667b4430d787be4f357bb6cab91fa426d950bb7
Ehshan Veerabangsa <eveerabangsa@gmail.com>
2015-04-18 19:08:05 +0100

    added test setUp

--- src/Tests/WebCrawlerTest.java
+++ src/Tests/WebCrawlerTest.java
@@ -1,21 +1,41 @@
 package Tests;
 
+import Implementations.WebCrawler;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import static org.junit.Assert.*;
 
 public class WebCrawlerTest {
 
-    @Before
-    public void setUp() {
+    private WebCrawler crawler;
+    private URL linksPage; //local html file to be tested
+    private URL webPage;  //web-page to be tested via http connection
+    private String filename; //name of database
+    private File database;
 
+    /**
+     * Test crawler setup
+     */
+    @Before
+    public void setUp() throws  MalformedURLException {
+        crawler = new WebCrawler();
+        linksPage = new URL("file:TestData/linksPage.html");
+        webPage = new URL ("http://ehshan.com/");
+        database = new File("filename");
     }
 
+    /**
+     * Closing crawler
+     */
     @After
     public void tearDown()  {
-
+        crawler = null;
     }
 
     @Test
