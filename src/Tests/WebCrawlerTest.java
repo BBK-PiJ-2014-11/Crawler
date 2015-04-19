9d5c2e62484c83a103b202b342e7eaedce20bc36
Ehshan Veerabangsa <eveerabangsa@gmail.com>
2015-04-19 22:54:01 +0100

    removed additional quotation marks from test data

--- src/Tests/WebCrawlerTest.java
+++ src/Tests/WebCrawlerTest.java
@@ -90,10 +90,10 @@ public class WebCrawlerTest {
         List urlList = crawler.getLinks(is);
 
         //converting URL to Strings
-        String home = "\""+webPage.toString()+"\"";
-        String portfolio = "\""+secondPage.toString()+"\"";
-        String contact = "\""+thirdPage.toString()+"\"";
-        String gallery = "\""+fourthPage.toString()+"\"";
+        String home = webPage.toString();
+        String portfolio = secondPage.toString();
+        String contact = thirdPage.toString();
+        String gallery = fourthPage.toString();
 
         //check list contain all links
         assertEquals(home,urlList.get(0));
@@ -113,10 +113,10 @@ public class WebCrawlerTest {
         List urlList = crawler.getLinks(is);
 
         //converting URL to Strings (urls form navigation bar)
-        String home = "\""+webPage.toString()+"\"";
-        String portfolio = "\""+secondPage.toString()+"\"";
-        String contact = "\""+thirdPage.toString()+"\"";
-        String gallery = "\""+fourthPage.toString()+"\"";
+        String home = webPage.toString();
+        String portfolio = secondPage.toString();
+        String contact = thirdPage.toString();
+        String gallery = fourthPage.toString();
 
         //check list contain all links
         assertEquals(home,urlList.get(0));
