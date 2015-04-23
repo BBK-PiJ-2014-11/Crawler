9bf1316a7fca7eeb7aced8051887404407a4e935
Ehshan Veerabangsa <eveerabangsa@gmail.com>
2015-04-23 21:45:06 +0100

    added class constructor

--- src/Implementations/CrawlerDB.java
+++ src/Implementations/CrawlerDB.java
@@ -2,7 +2,18 @@ package Implementations;
 
 import Interfaces.DB;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class CrawlerDB implements DB {
+
+
+    public CrawlerDB(Connection connection) throws SQLException {
+
+        connection .createStatement().execute("create table " + "tempTable" + "(priority int, url varchar(2000))");
+        connection .createStatement().execute("create table " + "resultsTable" + "(priority int, url varchar(2000))");
+    }
+
     @Override
     public void writeString(int priority, String url) {
 
