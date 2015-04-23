0dcbe16b5a63e331481fc6ffb07fd662b53a3b74
Ehshan Veerabangsa <eveerabangsa@gmail.com>
2015-04-23 20:43:42 +0100

    added test setup

--- src/Tests/CrawlerDBTest.java
+++ src/Tests/CrawlerDBTest.java
@@ -1,21 +1,48 @@
 package Tests;
 
+import Implementations.CrawlerDB;
+import Interfaces.DB;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import static org.junit.Assert.*;
 
 public class CrawlerDBTest {
 
-    @Before
-    public void setUp() throws Exception {
+    private Connection connection;
+    private DB database;
+    private Statement statement;
 
+    public static String driver = "org.apache.derby.jdbc.EmbeddedDriver";
+    public static String db_url = "jdbc:derby:memory:testdb;create=true";
+    public static String tableName = "testTable";
+
+    private String homepage;
+    private String portfolio;
+    private String contact;
+
+    @Before
+    public void setUp() throws ClassNotFoundException, SQLException {
+        database = new CrawlerDB(connection);
+
+        homepage = "http://ehshan.com/";
+        portfolio = "http://ehshan.com/portfolio/";
+        contact = "http://ehshan.com/contact/";
+
+        Class.forName(driver);
+        connection = DriverManager.getConnection(db_url);
+        connection.createStatement().execute("create table " + tableName + "(priority int, url varchar(2000))");
     }
 
     @After
     public void tearDown() throws Exception {
-
+        connection.close();
     }
 
     @Test
