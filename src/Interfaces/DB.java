5ee7c70d098f80c6cc5f795834dbcb327ced31ae
Ehshan Veerabangsa <eveerabangsa@gmail.com>
2015-04-23 19:48:38 +0100

    added DB interface

---
+++ src/Interfaces/DB.java
@@ -0,0 +1,24 @@
+package Interfaces;
+
+/**
+ * A class to create a database to store all unique crawled links
+ *
+ */
+public interface DB {
+
+    /**
+     * Writes the url (in String format) and its priority number to a table
+     *
+     * @param priority  the depth scraped
+     * @param url the link to be stored
+     */
+    void writeString(int priority, String url);
+
+    /**
+     * Checks if the current link is already present in designated table
+     *
+     * @param url the link to be checked
+     * @return true if link is already present table, false if not
+     */
+    boolean checkLinks (String url);
+}
