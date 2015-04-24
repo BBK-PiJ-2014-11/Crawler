559e33edafd12e15268de83dec0ba87b6b9431f6
Ehshan Veerabangsa <eveerabangsa@gmail.com>
2015-04-24 20:52:44 +0100

    updated test tearDown

--- src/Tests/CrawlerDBTest.java
+++ src/Tests/CrawlerDBTest.java
@@ -18,27 +18,34 @@ public class CrawlerDBTest {
 
     public static String driver = "org.apache.derby.jdbc.EmbeddedDriver";
     public static String db_url = "jdbc:derby:memory:testdb;create=true";
-    public static String tableName = "newTable";
+    public static String tableName = "tableName";
 
     private String homepage;
     private String portfolio;
     private String contact;
 
     @Before
-    public void setUp() throws ClassNotFoundException, SQLException {
+    public void setUp() throws ClassNotFoundException, SQLException, IllegalAccessException, InstantiationException {
         homepage = "http://ehshan.com/";
         portfolio = "http://ehshan.com/portfolio/";
         contact = "http://ehshan.com/contact/";
 
-        Class.forName(driver);
+        Class.forName(driver).newInstance();
         connection = DriverManager.getConnection(db_url);
-        //connection.createStatement().execute("create table " + tableName + "(priority int, url varchar(2000))");
+        System.out.println("connection created");
+        connection.createStatement().execute("create table " + tableName + "(priority int, url varchar(2000))");
         database = new CrawlerDB(connection);
     }
 
     @After
     public void tearDown() throws SQLException {
+        DatabaseMetaData dbm = connection.getMetaData();
+        ResultSet tables = dbm.getTables(null, null, tableName, null);
+        if (tables.next()) {
+            statement.execute("drop table "+ tableName);
+        }
         connection.close();
+//        statement.close();
     }
 
 
@@ -53,7 +60,7 @@ public class CrawlerDBTest {
         database.writeString(3, contact);
 
         //check the number of new records
-        result = statement.executeQuery("SELECT COUNT(*) FROM resultsTable");
+        result = statement.executeQuery("select COUNT(*) from "+ tableName);
         int rows = result.getInt("rows");
 
         assertEquals(3, rows);
